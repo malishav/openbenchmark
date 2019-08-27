c285888ed02a30f6ab1a71bc2e085124b437ea69
Mališa Vučinić <malishav@gmail.com>
2019-08-27 15:22:33 +0200

    Update KPI calculation.

--- process_kpis.py
+++ process_kpis.py
@@ -10,7 +10,7 @@ parser.add_argument('--log',
                     )
 
 
-def calculate_latency(inputFile, outputFile):
+def calculate_latency(inputFile):
     packetSentEvents = []
     packetReceivedEvents = []
     latencies = []
@@ -18,7 +18,7 @@ def calculate_latency(inputFile, outputFile):
     with open(inputFile, "r") as f:
         headerLine = json.loads(f.readline())
 
-        print "Processing KPIs for experiment {0} executed on {1}".format(headerLine['experimentId'], headerLine['date'])
+        print "Processing latency for experiment {0} executed on {1}".format(headerLine['experimentId'], headerLine['date'])
 
         # first fetch the events of interest
         for line in f:
@@ -35,6 +35,10 @@ def calculate_latency(inputFile, outputFile):
             tokenReceived = packetReceived['packetToken']
             for packetSent in packetSentEvents:
                 if packetSent['packetToken'] == tokenReceived:
+
+                    assert packetSent['source'] == packetReceived['destination']
+                    assert packetSent['destination'] == packetReceived['source']
+
                     latencies += [ {
                         "source" : packetSent['source'],
                         "destination" : packetSent['destination'],
@@ -43,23 +47,89 @@ def calculate_latency(inputFile, outputFile):
                     } ]
                     break
 
-        #print latencies
+    return { "latencies" : latencies }
 
-    #with open(outputFile, "a") as f:
-        # dump calculated latencies
-    #    pass
-    #    f.write('\n')
+def calculate_reliability(inputFile):
+    packetSentEvents = []
+    packetReceivedEvents = []
 
-def calculate_reliability(inputFile, outputFile):
-    pass
+    with open(inputFile, "r") as f:
+        headerLine = json.loads(f.readline())
+
+        print "Processing reliability for experiment {0} executed on {1}".format(headerLine['experimentId'],
+                                                                             headerLine['date'])
+
+        # first fetch the events of interest
+        for line in f:
+            candidate = json.loads(line)
+
+            # filter out the events of interest
+            if candidate['event'] == 'packetSent':
+                packetSentEvents += [candidate]
+            elif candidate['event'] == 'packetReceived':
+                packetReceivedEvents += [candidate]
+
+    reliability = len(packetReceivedEvents) / float(len(packetSentEvents))
+
+    return {"reliability": reliability}
+
+def calculate_join_times(inputFile):
+    joinInstants = []
+
+
+    with open(inputFile, "r") as f:
+        headerLine = json.loads(f.readline())
+
+        print "Processing join times for experiment {0} executed on {1}".format(headerLine['experimentId'],
+                                                                             headerLine['date'])
+
+        # first fetch the events of interest
+        for line in f:
+            candidate = json.loads(line)
+
+            # filter out the events of interest
+            if candidate['event'] == 'secureJoinCompleted':
+                joinInstants += [candidate['timestamp']]
+
+    return {"joinInstants": joinInstants}
+
+def calculate_sync_times(inputFile):
+    syncInstants = []
+
+
+    with open(inputFile, "r") as f:
+        headerLine = json.loads(f.readline())
+
+        print "Processing sync times for experiment {0} executed on {1}".format(headerLine['experimentId'],
+                                                                             headerLine['date'])
+
+        # first fetch the events of interest
+        for line in f:
+            candidate = json.loads(line)
+
+            # filter out the events of interest
+            if candidate['event'] == 'synchronizationCompleted':
+                syncInstants += [candidate['timestamp']]
+
+    return {"syncInstants": syncInstants}
 
 def main():
 
     args = parser.parse_args()
     inputFile = args.log
     outputFile = args.log + ".kpi"
-    calculate_latency(inputFile, outputFile)
-    calculate_reliability(inputFile, outputFile)
+
+    kpis = {}
+
+    kpis.update(calculate_latency(inputFile))
+    kpis.update(calculate_reliability(inputFile))
+    kpis.update(calculate_join_times(inputFile))
+    kpis.update(calculate_sync_times(inputFile))
+
+    with open(outputFile, "w") as f:
+        json.dump(kpis, f)
+        f.write('\n')
+
 
 if __name__ == '__main__':
     main()
