ca70602cc75d60e9c6151164af9c4a831ce40870
Mališa Vučinić <malishav@gmail.com>
2019-09-16 15:21:01 +0200

    process_kpis: Update reliability calculation to split for upstream/downstream.

--- process_kpis.py
+++ process_kpis.py
@@ -103,76 +103,104 @@ def calculate_latency(inputDir):
     return returnDict
 
 def calculate_reliability(inputDir):
-    reliabilitySent = []
-    reliabilityCommanded = []
+    reliabilitySentUpstream = []
+    reliabilitySentDownstream = []
+    reliabilitySentP2P = []
+    reliabilitySerialPort = []
     returnDict = {}
 
     for inputFile in glob.glob(os.path.join(inputDir, '*.log')):
-        packetSentEvents = []
-        packetReceivedEvents = []
-        commandSendPacketEvents = []
+
+        packetSentTokensUpstream = set()
+        packetReceivedTokensUpstream = set()
+        packetSentTokensDownstream = set()
+        packetReceivedTokensDownstream = set()
+        packetSentTokensP2P = set()
+        packetReceivedTokensP2P = set()
+        commandSendPacketTokens = set()
 
         with open(inputFile, "r") as f:
             headerLine = json.loads(f.readline())
 
-            print "Processing reliability for experiment {0} executed on {1}".format(headerLine['experimentId'],
-                                                                                 headerLine['date'])
+            # root is by convention always openbenchmark00 node
+            root = headerLine['nodes']['openbenchmark00']['eui64']
 
+            print "Processing reliability for experiment {0} executed on {1}. Root eui64={2}".format(headerLine['experimentId'],
+                                                                                 headerLine['date'], root)
             # first fetch the events of interest
             for line in f:
                 candidate = json.loads(line)
 
                 # filter out the events of interest
                 if candidate['event'] == 'packetSent':
-                    packetSentEvents += [candidate]
+                    token = tuple(candidate['packetToken'])
+                    if candidate['destination'] == root:
+                        # upstream packet
+                        packetSentTokensUpstream.add(token)
+                    elif candidate['source'] == root:
+                        # downstream packet
+                        packetSentTokensDownstream.add(token)
+                    else:
+                        # P2P packet
+                        packetSentTokensP2P.add(token)
+
                 elif candidate['event'] == 'packetReceived':
-                    packetReceivedEvents += [candidate]
+                    token = tuple(candidate['packetToken'])
+                    if candidate['source'] == root:
+                        # upstream packet
+                        packetReceivedTokensUpstream.add(token)
+                    elif candidate['destination'] == root:
+                        # downstream packet
+                        packetReceivedTokensDownstream.add(token)
+                    else:
+                        # P2P packet
+                        packetReceivedTokensP2P.add(token)
+
                 elif candidate['event'] == 'command' and candidate['type'] == 'sendPacket':
-                    commandSendPacketEvents += [candidate]
 
-        # filter out the duplicates in packetReceivedEvents
-        packetReceivedTokens = []
-        for packetReceivedEvent in packetReceivedEvents:
-            packetReceivedTokens += [tuple(packetReceivedEvent['packetToken'])]
-        packetReceivedTokens = set(packetReceivedTokens)
+                    token = tuple(candidate['packetToken'])
+                    commandSendPacketTokens.add(token)
 
-        # filter out the duplicates in packetSentEvents
-        packetSentTokens = []
-        for packetSentEvent in packetSentEvents:
-            packetSentTokens += [tuple(packetSentEvent['packetToken'])]
-        packetSentTokens = set(packetSentTokens)
+        reliabilitySentUpstream     += [1 - len(packetSentTokensUpstream - packetReceivedTokensUpstream) / float(len(packetSentTokensUpstream))] if len(packetSentTokensUpstream) else []
+        reliabilitySentDownstream   += [1 - len(packetSentTokensDownstream - packetReceivedTokensDownstream) / float(len(packetSentTokensDownstream))] if len(packetSentTokensDownstream) else []
+        reliabilitySentP2P          += [1 - len(packetSentTokensP2P - packetReceivedTokensP2P) / float(len(packetSentTokensP2P))] if len(packetSentTokensP2P) else []
 
-        commandSendPacketTokens = []
-        for command in commandSendPacketEvents:
-            commandSendPacketTokens += [tuple(command['packetToken'])]
-        commandSendPacketTokens = set(commandSendPacketTokens)
-
-        assert commandSendPacketTokens >= packetSentTokens
-        assert commandSendPacketTokens >= packetReceivedTokens
-
-        reliabilitySent += [1 - len(packetSentTokens - packetReceivedTokens) / float(len(packetSentTokens))]
-        reliabilityCommanded += [1 - len(commandSendPacketTokens - packetReceivedTokens) / float(len(commandSendPacketTokens))]
+        reliabilitySerialPort        += [1 - len(commandSendPacketTokens - (packetSentTokensUpstream | packetSentTokensDownstream | packetSentTokensP2P)) / float(len(commandSendPacketTokens))]
 
         print "Experiment {0} Number of packets commanded: {1}".format(headerLine['experimentId'], len(commandSendPacketTokens))
 
 
-    returnDict['reliabilitySent'] = {
-                                        'mean' : mean(reliabilitySent),
-                                        'min' : min(reliabilitySent),
-                                        'max' : max(reliabilitySent),
-                                        '99%' : numpy.percentile(reliabilitySent, 99)
-                                    }
+    if len(reliabilitySentUpstream):
+        returnDict['reliabilitySentUpstream'] = {
+                                            'mean' : mean(reliabilitySentUpstream),
+                                            'min' : min(reliabilitySentUpstream),
+                                            'max' : max(reliabilitySentUpstream),
+                                            '99%' : numpy.percentile(reliabilitySentUpstream, 99)
+                                        }
+    if len(reliabilitySentDownstream):
+        returnDict['reliabilitySentDownstream'] = {
+                                            'mean' : mean(reliabilitySentDownstream),
+                                            'min' : min(reliabilitySentDownstream),
+                                            'max' : max(reliabilitySentDownstream),
+                                            '99%' : numpy.percentile(reliabilitySentDownstream, 99)
+                                        }
 
-    returnDict['reliabilityCommanded'] = {
-                                        'mean' : mean(reliabilityCommanded),
-                                        'min' : min(reliabilityCommanded),
-                                        'max' : max(reliabilityCommanded),
-                                        '99%' : numpy.percentile(reliabilityCommanded, 99)
-                                    }
+    if len(reliabilitySentP2P):
+        returnDict['reliabilitySentP2P'] = {
+                                            'mean' : mean(reliabilitySentP2P),
+                                            'min' : min(reliabilitySentP2P),
+                                            'max' : max(reliabilitySentP2P),
+                                            '99%' : numpy.percentile(reliabilitySentP2P, 99)
+                                        }
 
+    if len(reliabilitySerialPort):
+        returnDict['reliabilitySerialPort'] = {
+                                            'mean' : mean(reliabilitySerialPort),
+                                            'min' : min(reliabilitySerialPort),
+                                            'max' : max(reliabilitySerialPort),
+                                            '99%' : numpy.percentile(reliabilitySerialPort, 99)
+                                        }
 
-    print "Reliabilities commanded: {0}".format(reliabilityCommanded)
-    print "Reliabilities sent: {0}".format(reliabilitySent)
     print returnDict
 
     return returnDict
